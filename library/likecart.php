cab25afeb02bb53a1642a449f0f9aadb0378638f
jung-hye-ji <93874150+jung-hye-ji@users.noreply.github.com>
2021-11-08 01:08:02 +0900

    Update likecart.php

--- library/likecart.php
+++ library/likecart.php
@@ -8,6 +8,7 @@
 		<title>찜 목록</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width-device-width, initial-scale-1">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
